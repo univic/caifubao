f8268eeb8cad46d75b48b521d6d29e20f06f6108
univic <xyzhgwf@hotmail.com>
2022-06-21 22:45:38 +0800

    working on factor processor

--- caifubao-backend/app/lib/factor_facotry/processors/fq_factor.py
+++ caifubao-backend/app/lib/factor_facotry/processors/fq_factor.py
@@ -1,16 +1,40 @@
+import datetime
+import pandas as pd
+import numpy as np
+
 from app.utilities import freshness_meta_helper
+from app.model.stock import StockDailyQuote
 
 
 def calc_fq_factor(processor_obj):
     most_recent_factor_date = freshness_meta_helper.read_freshness_meta(processor_obj.stock_obj, 'fq_factor')
+    raw_df = processor_obj.quote_df
+    # most_recent_factor_date = datetime.datetime(2022, 6, 20, 0, 0, 0)
+    # if found existing factor, slice the df to reduce calculate work
+    if most_recent_factor_date:
+        # get previous quote data to make cumprod possible
+        head_index = raw_df.index.get_loc(most_recent_factor_date) - 1
+        df = raw_df.iloc[head_index:][:]
+    else:
+        df = raw_df
 
-    processor_obj.quote_df["fq_factor"] = (processor_obj.quote_df["close"] / processor_obj.quote_df["previous_close"]).cumprod()
-    processor_obj.quote_df["close_hfq"] = (processor_obj.quote_df["fq_factor"] * processor_obj.quote_df.iloc[0]['previous_close']).round(
+    # do the maths
+    df["fq_factor"] = (df["close"] / df["previous_close"]).cumprod()
+    df["close_hfq"] = (df["fq_factor"] * raw_df.iloc[0]['previous_close']).round(
         decimals=4)
-    processor_obj.quote_df["open_hfq"] = (processor_obj.quote_df["open"] * (processor_obj.quote_df["close_hfq"] / processor_obj.quote_df["close"])).round(
+    df["open_hfq"] = (df["open"] * (df["close_hfq"] / df["close"])).round(
         decimals=4)
-    processor_obj.quote_df["high_hfq"] = (processor_obj.quote_df["high"] * (processor_obj.quote_df["close_hfq"] / processor_obj.quote_df["close"])).round(
+    df["high_hfq"] = (df["high"] * (df["close_hfq"] / df["close"])).round(
         decimals=4)
-    processor_obj.quote_df["low_hfq"] = (processor_obj.quote_df["low"] * (processor_obj.quote_df["close_hfq"] / processor_obj.quote_df["close"])).round(
+    df["low_hfq"] = (df["low"] * (df["close_hfq"] / df["close"])).round(
         decimals=4)
     pass
+
+    # update database
+    for i, row in df.iterrows():
+        field_list = ["fq_factor", "close_hfq", "open_hfq", "high_hfq", "low_hfq"]
+        quote_obj = StockDailyQuote.objects(code=row["code"], date=i).first()
+        for field in field_list:
+            quote_obj[field] = row[field]
+        quote_obj.save()
+
