62e7caf0de1ff99355539bc6c4a60460cfeec38f
univic <xyzhgwf@hotmail.com>
2022-07-01 00:22:32 +0800

    working on strategy scheme

--- caifubao-backend/app/lib/factor_facotry/processors/__init__.py
+++ caifubao-backend/app/lib/factor_facotry/processors/__init__.py
@@ -1,4 +1,17 @@
 from app.utilities import freshness_meta_helper
+from app.lib.factor_facotry.processors import fq_factor, moving_average
+
+
+factor_registry = {
+    "FQ_FACTOR": {
+        'processor_object': fq_factor.FQFactorProcessor,
+        'handler': 'perform_factor_calc'
+    },
+    "MA_10": {
+        'processor_object': moving_average.MovingAverageFactorProcessor,
+        'handler': 'perform_factor_calc'
+    }
+}
 
 
 class FactorProcessor(object):
--- caifubao-backend/app/lib/factor_facotry/processors/moving_average.py
+++ caifubao-backend/app/lib/factor_facotry/processors/moving_average.py
@@ -6,14 +6,20 @@ from app.lib.factor_facotry.processors import FactorProcessor
 
 class MovingAverageFactorProcessor(FactorProcessor):
 
-    def __init__(self, stock_obj, ma_days):
+    def __init__(self, stock_obj):
         super().__init__(stock_obj)
-        self.ma_days = ma_days
-        self.factor_name = f'ma_{ma_days}'
+        self.ma_days = 0
+        self.factor_name = ''
 
     def perform_factor_calc(self):
+
         self.stock_obj.quote_df[self.factor_name] = talib.MA(self.stock_obj.quote_df[self.factor_name],
                                                              timeperiod=self.ma_days)
+
+    def perform_ma_10_calc(self):
+        self.ma_days = 10
+        self.factor_name = f'ma_{self.ma_days}'
+        self.perform_factor_calc()
 
     def read_existing_factors(self):
         pass
