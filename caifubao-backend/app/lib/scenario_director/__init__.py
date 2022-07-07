615f01e2a53bffa680ccd3881e43ae31482f5b7e
univic <xyzhgwf@hotmail.com>
2022-07-07 23:57:51 +0800

    working on scenarios

--- caifubao-backend/app/lib/scenario_director/__init__.py
+++ caifubao-backend/app/lib/scenario_director/__init__.py
@@ -1,4 +1,4 @@
-from app.schemes import scenario
+from app.schemes import scenario as scenario_schemes
 from app.lib.scenario_director import scenarios
 """
 how would you like the world to turn?
@@ -16,13 +16,15 @@ how would you like the world to turn?
 class ScenarioDirector(object):
 
     def __init__(self, scenario_scheme_name):
+        self.scenario = None
+        self.scenario_scheme = None
         self.scenario_scheme_name = scenario_scheme_name
 
     def load_scenario_scheme(self):
-        scenario_director_obj = getattr(scenarios, self.scenario_scheme_name)
+        self.scenario_scheme = getattr(scenario_schemes, self.scenario_scheme_name)
 
     def load_scenario(self):
-        pass
+        self.scenario = getattr(scenarios, self.scenario_scheme_name)
 
     def run_scenario(self):
         pass
