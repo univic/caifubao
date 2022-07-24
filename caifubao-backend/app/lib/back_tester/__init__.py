eeac4468413de4141cbb5e04fc89f421a1e5568c
univic <xyzhgwf@hotmail.com>
2022-07-24 23:01:00 +0800

    working on backtester

--- caifubao-backend/app/lib/back_tester/__init__.py
+++ caifubao-backend/app/lib/back_tester/__init__.py
@@ -1,23 +1,32 @@
 from app.lib.db_tool import mongoengine_tool
 from app.lib.strategy import StrategyInterpreter
+from app.utilities import trading_day_helper
 
 
 class BasicBackTester(object):
 
     def __init__(self, scenario, portfolio, strategy):
-        self.scenario = None
+        self.scenario = scenario
         self.trading_day_list = []
         self.stock_list = []
         self.strategy_interpreter = StrategyInterpreter(strategy)
 
     def run_back_test(self):
         print('backtest running')
+        self.get_backtest_date_range()
+        # self.before_back_test()
+        # self.after_back_test()
 
     def before_back_test(self):
         mongoengine_tool.connect_to_db()
 
     def after_back_test(self):
         mongoengine_tool.disconnect_from_db()
+
+    def get_backtest_date_range(self):
+        start_date = self.scenario.start_date
+        trade_calendar = trading_day_helper.get_a_stock_market_trade_calendar()
+        print(start_date)
 
     def setup_backtest(self, scenario):
         self.scenario = scenario
