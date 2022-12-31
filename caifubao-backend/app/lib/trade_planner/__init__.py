41c5ca8a7c28866d9a8a57903c5ea0896d2744ba
univic <xyzhgwf@hotmail.com>
2022-12-31 22:30:51 +0800

    progress sync

--- caifubao-backend/app/lib/trade_planner/__init__.py
+++ caifubao-backend/app/lib/trade_planner/__init__.py
@@ -7,6 +7,14 @@ logger = logging.getLogger()
 
 
 class TradePlanner(object):
+    """
+    for each trading day, check all the available trading opportunities
+    choose most suitable trading opportunity for each portfolio
+    a suitable trading opportunity must meet following requirements:
+    - the trade must be accomplishable
+    - the trade is best suitable for the portfolio's risk preference
+    - the trade have the best chance for profiting
+    """
     def __init__(self, stock, processor_name):
         module_name = 'TradePlanner'
         meta_type = 'trade_planner'
