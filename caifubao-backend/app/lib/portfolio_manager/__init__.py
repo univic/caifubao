59b27d6fb0216b73e3fef299769975a1fa104f18
univic <xyzhgwf@hotmail.com>
2023-01-10 23:44:31 +0800

    working on portfolio manager

--- caifubao-backend/app/lib/portfolio_manager/__init__.py
+++ caifubao-backend/app/lib/portfolio_manager/__init__.py
@@ -2,5 +2,14 @@
 
 class PortfolioManager(object):
 
-    def __init__(self):
+    def __init__(self, portfolio_id):
+        pass
+
+    def check_trade_opportunities(self):
+        pass
+
+    def make_trade_decision(self):
+        pass
+
+    def update_portfolio_info(self):
         pass
