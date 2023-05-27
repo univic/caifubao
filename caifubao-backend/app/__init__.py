49e748b0c14f97c88e6a505142d96dcb3418c71c
Wenfeng <xyzhgwf@hotmail.com>
2023-05-27 14:09:03 +0000

    working on structure adjustment

--- caifubao-backend/app/__init__.py
+++ caifubao-backend/app/__init__.py
@@ -3,24 +3,15 @@
 # Date: 2022-02-13
 
 
-from flask import Flask
 from app.utilities.logger import create_logger
-from app.conf import app_config
-from app.api import register_blueprint
 from app.lib.db_tool.mongoengine_tool import db_init
 from app.lib.datahub import Datahub
-from app.extensions import config_extensions
+
 
 logger = create_logger()
 
 
 def create_app():
     logger.info('Stellaris initializing')
-    app = Flask(__name__)
-    app.debug = True
-    app.config.from_object(app_config)
-    register_blueprint(app)
-    config_extensions(app)
-    db_init(app)
     datahub = Datahub()
     datahub.initialize()
--- caifubao-backend/app/lib/signal_man/__init__.py
+++ caifubao-backend/app/lib/signal_man/__init__.py
@@ -12,3 +12,8 @@ class SignalMan(GeneralFactory):
         meta_type = 'signal_processor'
         processor_registry = processors.registry
         super().__init__(stock, module_name, meta_type, processor_registry, processor_name_list)
+
+
+if __name__ == "__main__":
+    signal_man = SignalMan()
+    pass
