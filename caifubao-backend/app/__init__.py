2744b53527c38b221c1f5ce5c2e11d97169aa960
Wenfeng <xyzhgwf@hotmail.com>
2024-02-13 14:27:43 +0000

    change start up sequence

--- caifubao-backend/app/__init__.py
+++ caifubao-backend/app/__init__.py
@@ -4,7 +4,11 @@
 
 
 import logging
-from app.lib.dispatcher import MainDispatcher
+from app.lib.datahub import Datahub
+from app.lib.task_controller import task_controller
+from app.lib.db_watcher.mongoengine_tool import db_watcher
+# from app.lib.dispatcher import MainDispatcher
+from app.lib.strategy import strategy_director
 
 
 logger = logging.getLogger(__name__)
@@ -12,5 +16,25 @@ logger = logging.getLogger(__name__)
 
 def create_app():
     logger.info('Stellaris initializing')
-    MainDispatcher.dispatch()
 
+    # Establish DB Connection
+    db_watcher.initialize()
+    db_watcher.connect_to_db()
+
+    # Start Datahub
+    datahub = Datahub()
+    datahub.start()
+
+    # Start Task Controller
+    task_controller.initialize()
+
+    # Start web server
+
+
+    # Load Scenario and Strategy
+    strategy_director.load_strategy("Strategy01")
+
+    # MainDispatcher.dispatch()
+
+    while True:
+        pass
